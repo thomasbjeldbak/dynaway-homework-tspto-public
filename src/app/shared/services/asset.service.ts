69e1ee8e49472f742ef6f18757d143cc1cadc9f9
Josef Kriz <jkr@dynaway.com>
2021-03-18 08:10:09 +0100

    add a slight chance for the asset fetch to fail

--- src/app/shared/services/asset.service.ts
+++ src/app/shared/services/asset.service.ts
@@ -2,19 +2,24 @@ import { Injectable } from '@angular/core'
 import { Observable, of } from 'rxjs'
 import { Asset } from '../models/asset.model'
 import { mockAssetHttpResponse } from './asset.test'
-import { delay } from 'rxjs/operators'
+import { delay, tap } from 'rxjs/operators'
 import { getRandomInt } from '../functions'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AssetService {
 
-  constructor() { }
+  constructor() {
+  }
 
   getAll(): Observable<Asset[]> {
     return of(mockAssetHttpResponse).pipe(
       delay(getRandomInt(1000) + 500), // fake random http delay,
+      tap(() => { // a small chance for the data fetch to error
+        if (getRandomInt(10) % 10 === 0) throw Error('Http error')
+      }),
     )
   }
 }
+
